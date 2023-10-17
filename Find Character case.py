0198c5ac6827fc66291f6f7414dfa68ae3f11934
Varun Mohankumar <varunmj@users.noreply.github.com>
2023-10-17 13:14:41 -0500

    Update Find Character case.py

--- Find Character case.py
+++ Find Character case.py
@@ -1,3 +1,8 @@
+# Write a program that takes a character as input and prints 1,0,-1 according to the following rules:
+# 1 if the character is an uppercase
+# 0 if the character is lowercase
+# -1 if the character is not an alphabet
+
 a  = input()
 
 if a.isalpha() == True:
